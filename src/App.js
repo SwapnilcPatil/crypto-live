f9c404196c496431c729958aa3fa4e9bf544ba3e
swapcp1000 <swapcp.1000@gmail.com>
2022-01-02 04:51:18 +0530

    Header and banner is done

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import { makeStyles } from '@material-ui/core';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Coinpage from './Pages/Coinpage';
+import Homepage from './Pages/Homepage';
+
+function App() {
+
+  const useStyles = makeStyles(() =>({
+    App: {
+      backgroundColor:'#14161a',
+      color:"white",
+      minHeight: "100vh",
+    }
+  }));
+
+  const classes = useStyles();
+
+  return (
+   <BrowserRouter>
+   <div className={classes.App}>
+   <Header/>
+   <Routes>
+   <Route path="/crypto-live/" element={<Homepage/>}/>
+   <Route path="/crypto-live/coins/:id" element={<Coinpage/>}/>
+   </Routes>
+   </div>
+   </BrowserRouter>
+  );
+}
+
+export default App;
